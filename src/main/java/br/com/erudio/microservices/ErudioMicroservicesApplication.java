27ffe12b4c5367ebc0665fc15de3bcff1268b264
douglasfranca19 <francadouglas19@gmail.com>
2025-02-16 12:47:43 -0300

    Primeira subida para teste

---
+++ src/main/java/br/com/erudio/microservices/ErudioMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErudioMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErudioMicroservicesApplication.class, args);
+	}
+
+}
